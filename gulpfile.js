878146246ea059ee50ce8918e2a4d10d4a165310
Sofie Blom <ssofieblom@hotmail.com>
2021-10-29 14:00:14 +0200

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+var gulp = require("gulp");
+var sass = require("gulp-sass")(require("sass"));
+
+gulp.task("compile", function () {
+  return gulp.src("scss/main.scss").pipe(sass()).pipe(gulp.dest("css"));
+});
+
+gulp.task("watch-sass", function () {
+  gulp.watch("scss/main.scss", gulp.series("compile"));
+});
+
+gulp.task("default", gulp.series("compile", "watch-sass"), function () {});
+
